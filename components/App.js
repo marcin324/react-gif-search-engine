1b8b1b40ae5c985fac608027db0251b90025c87f
Marcin Olszowski <olszyna38@gmail.com>
2018-10-07 23:30:37 +0200

    Add .gitignore

--- components/App.js
+++ components/App.js
@@ -49,7 +49,7 @@ App = React.createClass({
             width: '90%'
         };
 
-        return (
+        return ()
           	<div style={styles}>
                 <h1>Wyszukiwarka GIFow!</h1>
                 <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
